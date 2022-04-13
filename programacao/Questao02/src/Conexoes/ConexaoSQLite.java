10c2dee480379d92364c29a817bb3a22801b8dc4
fagnersilva2 <fagnersyllwa@gmail.com>
2022-04-13 20:24:16 -0300

    Ajeitando o arquivo jar

---
+++ programacao/Questao02/src/Conexoes/ConexaoSQLite.java
@@ -0,0 +1,68 @@
+
+package Conexoes;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Fagner Silva
+ */
+public class ConexaoSQLite {
+
+    private Connection conexao;
+
+    public boolean conectar() {
+
+        try {
+
+            String url = "jdbc:sqlite:banco_sqlite.db";
+
+            this.conexao = DriverManager.getConnection(url);
+
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+            return false;
+        }
+       // System.out.println("conectou");
+        return true;
+    }
+
+    public boolean desconectar() {
+
+        try {
+            if (this.conexao.isClosed() == false) {
+                this.conexao.close();
+            }
+
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+            return false;
+        }
+       // System.out.println("desconectou");
+        return true;
+    }
+
+    public Statement criarStatement() {
+        try {
+            return this.conexao.createStatement();
+        } catch (SQLException e) {
+            return null;
+        }
+    }
+
+    public PreparedStatement criarPreparedStatement(String sql) {
+        try {
+            return this.conexao.prepareStatement(sql);
+        } catch (SQLException e) {
+            return null;
+        }
+    }
+
+    public Connection getConexao() {
+        return this.conexao;
+    }
+ 
+}
---
+++ programacao/Questao02/src/app/Numero.java
@@ -0,0 +1,38 @@
+package app;
+/**
+ *
+ * @author Fagner Silva
+ */
+public class Numero {
+
+  private int id;
+    private int numero;
+    private String fatorial;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getFatorial() {
+        return fatorial;
+    }
+
+    public void setFatorial(String fatorial) {
+        this.fatorial = fatorial;
+    }
+
+
+
+}
